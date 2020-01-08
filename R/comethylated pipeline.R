85059b4ddad4a58aeaae50c4f84f4bc5ddba058c
Charles Mordaunt <cemordaunt@ucdavis.edu>
2020-01-08 14:14:57 -0800

    Edit getCpGs() and add getRegions()

--- R/comethylated pipeline.R
+++ R/comethylated pipeline.R
@@ -9,7 +9,7 @@ sapply(c("tidyverse", "bsseq", "dmrseq", "WGCNA", "openxlsx"), require, characte
 # Functions ####
 getCpGs <- function(colData, path = getwd(), pattern = "*CpG_report.txt.gz", 
                     chroms = c(paste("chr", 1:22, sep = ""), "chrX", "chrY", "chrM"), BPPARAM = MulticoreParam(10), 
-                    cov = 2, perSample = 0.75, saveBSseq = TRUE, outfile = "Filtered_BSseq.rds", verbose = TRUE){
+                    cov = 2, perSample = 0.75, save = TRUE, file = "Filtered_BSseq.rds", verbose = TRUE){
         if(verbose){
                 message("[getCpGs] Loading CpG-level data")
         }
@@ -17,8 +17,8 @@ getCpGs <- function(colData, path = getwd(), pattern = "*CpG_report.txt.gz",
         loci <- bsseq:::.readBismarkAsFWGRanges(files[1]) %>% chrSelectBSseq(seqnames = chroms)
         bs <- read.bismark(files, loci = loci, colData = colData, BPPARAM = BPPARAM, verbose = verbose)
         if(verbose){
-                message("[getCpGs] Filtering CpG-level data for loci with at least ", cov, " reads in ", perSample * 100, 
-                        "% of samples")
+                message("[getCpGs] Filtering CpG-level data for loci with at least ", cov, " reads in at least ", 
+                        perSample * 100, "% of samples")
         }
         covSample <- (getCoverage(bs) >= cov) %>% DelayedMatrixStats::rowSums2()
         bs <- bs[covSample >= (perSample * ncol(bs)),]
@@ -26,16 +26,55 @@ getCpGs <- function(colData, path = getwd(), pattern = "*CpG_report.txt.gz",
                 message("[getCpGs] Final BSseq Object:")
                 print(bs)
         }
-        if(saveBSseq){
+        if(save){
                 if(verbose){
-                        message("[getCpGs] Saving BSseq Object as ", outfile)
+                        message("[getCpGs] Saving BSseq object as ", file)
                 }
-                saveRDS(bs, file = outfile)
+                saveRDS(bs, file = file)
         }
         if(verbose){
                 message("[getCpGs] Complete!")
         }
         return(bs)
+}
+
+getRegions <- function(bs, maxGap = 150, n = 3, covMin = 10, methSD = 0.05, save = TRUE, file = "Filtered_Regions.txt",
+                       verbose = TRUE){
+        if(verbose){
+                message("[getRegions] Calling regions when at least ", n, " CpGs are no more than ", maxGap, " bases apart")
+        }
+        regions <- bsseq:::regionFinder3(x = as.integer(rep(1, length(bs))), chr = as.character(seqnames(bs)), 
+                                         positions = start(bs), maxGap = maxGap, verbose = FALSE)[["up"]]
+        regions <- regions[regions$n >= n,]
+        regions$chr <- as.character(regions$chr)
+        regions$width <- regions$end - regions$start
+        if(verbose){
+                message("[getRegions] Filtering regions for at least ", covMin, 
+                        " reads in all samples and methylation SD of at least ", methSD * 100, "%")
+        }
+        cov <- getCoverage(bs, regions = regions[,c("chr", "start", "end")], what = "perRegionTotal")
+        regions$covMean <- DelayedMatrixStats::rowMeans2(cov)
+        regions$covSD <- DelayedMatrixStats::rowSds(cov)
+        regions$covMin <- DelayedArray::rowMins(cov)
+        regions <- regions[regions$covMin >= covMin,]
+        meth <- getMeth(bs, regions = regions[,c("chr", "start", "end")], type = "raw", what = "perRegion")
+        regions$methMean <- DelayedMatrixStats::rowMeans2(meth)
+        regions$methSD <- DelayedMatrixStats::rowSds(meth)
+        regions <- regions[regions$methSD >= methSD,]
+        regions$RegionID <- paste("Region", 1:nrow(regions), sep = "_")
+        regions$width <- regions$end - regions$start
+        regions <- regions[,c("RegionID", "chr", "start", "end", "width", "n", "covMin", "covMean", "covSD", "methMean", 
+                              "methSD")]
+        if(save){
+                if(verbose){
+                        message("[getRegions] Saving regions as ", file)
+                }
+                write.table(regions, file = file, quote = FALSE, sep = "\t", row.names = FALSE)
+        }
+        if(verbose){
+                message("[getRegions] Complete!")
+        }
+        return(regions)
 }
 
 # Read and Filter Bismark CpG Reports ###
@@ -44,20 +83,6 @@ colData <- colData[4:9,] # subset for testing
 bs <- getCpGs(colData)
 
 # Call and Filter Regions ####
-regions <- bsseq:::regionFinder3(x = as.integer(rep(1, length(bs))), chr = as.character(seqnames(bs)), 
-                                 positions = start(bs), maxGap = 150, verbose = FALSE)[["up"]] %>%
-        subset(n >= 3, select = c("chr", "start", "end", "n"))
-regions$chr <- as.character(regions$chr)
-regions$width <- regions$end - regions$start
-regions$minCov <- getCoverage(bs, regions = regions[,c("chr", "start", "end")], what = "perRegionTotal") %>% 
-        DelayedArray::rowMins()
-regions$SDs <- getMeth(bs, regions = regions[,c("chr", "start", "end")], type = "raw", what = "perRegion") %>% 
-        DelayedMatrixStats::rowSds()
-regions <- subset(regions, minCov >= 10 & SDs >= 0.05)
-regions$RegionID <- paste("Region", 1:nrow(regions), sep = "_")
-write.table(regions, file = "WGCNA_males/Males WGCNA minCov 10 SD 5 Filtered regions.txt", quote = FALSE, sep = "\t",
-            row.names = FALSE)
-
-
+regions <- getRegions(bs)
 
 
