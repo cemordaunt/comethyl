af73032f675ecc1067de5a49caf1d8a3c8615020
Charles Mordaunt <cemordaunt@ucdavis.edu>
2020-01-20 11:46:14 -0800

    Clean up function names

--- R/comethyl.R
+++ R/comethyl.R
@@ -28,48 +28,48 @@ getCpGs <- function(colData, path = getwd(), pattern = "*CpG_report.txt.gz",
         return(bs)
 }
 
-.n <- function(covSample, nSample){
+.nCpGsBySample <- function(covSample, nSample){
         n <- length(covSample[covSample >= nSample])
         return(n)
 }
 
-.nCpGs <- function(bsCov, cov, nSample){
+.nCpGsByCov <- function(bsCov, cov, nSample){
         covSample <- (bsCov >= cov) %>% DelayedMatrixStats::rowSums2()
-        nCpGs <- sapply(nSample, FUN = .n, covSample = covSample)
+        nCpGs <- sapply(nSample, FUN = .nCpGsBySample, covSample = covSample)
         return(nCpGs)
 }
 
-getCpGfilterTotals <- function(bs, cov = seq(0,10,1), perSample = seq(0.5,1,0.05), save = TRUE, file = "CpG_Filter_Totals.txt",
-                               verbose = TRUE){
+getCpGtotals <- function(bs, cov = seq(0,10,1), perSample = seq(0.5,1,0.05), save = TRUE, file = "CpG_Totals.txt",
+                         verbose = TRUE){
         if(verbose){
-                message("[getCpGfilterTotals] Calculating CpG totals at specified cov and perSample cutoffs")
+                message("[getCpGtotals] Calculating CpG totals at specified cov and perSample cutoffs")
         }
         nSample <- (perSample * ncol(bs)) %>% ceiling()
         bsCov <- getCoverage(bs)
-        nCpGs <- sapply(cov, FUN = .nCpGs, bsCov = bsCov, nSample = nSample)
+        nCpGs <- sapply(cov, FUN = .nCpGsByCov, bsCov = bsCov, nSample = nSample)
         nCpGs <- as.integer(nCpGs)
         perCpGs <- (nCpGs / length(bs)) %>% round(digits = 5)
-        CpGfilterTotals <- data.frame(cov = rep(cov, each = length(perSample)), perSample = rep(perSample, times = length(cov)), 
-                                      nSample = rep(nSample, times = length(cov)), nCpGs_M = nCpGs / 10^6, perCpGs = perCpGs)
+        CpGtotals <- data.frame(cov = rep(cov, each = length(perSample)), perSample = rep(perSample, times = length(cov)), 
+                                nSample = rep(nSample, times = length(cov)), nCpGs_M = nCpGs / 10^6, perCpGs = perCpGs)
         if(save){
                 if(verbose){
-                        message("[getCpGfilterTotals] Saving file as ", file)
+                        message("[getCpGtotals] Saving file as ", file)
                 }
-                write.table(CpGfilterTotals, file = file, sep = "\t", row.names = FALSE)
+                write.table(CpGtotals, file = file, sep = "\t", row.names = FALSE)
         }
-        return(CpGfilterTotals)
+        return(CpGtotals)
 }
 
-plotCpGfilterTotals <- function(CpGfilterTotals, nBreaks = 4, legend.position = c(1.08,0.73), save = TRUE, 
-                                file = "CpG_Filter_Totals.pdf", width = 11, height = 4.25, verbose = TRUE){
+plotCpGtotals <- function(CpGtotals, nBreaks = 4, legend.position = c(1.08,0.73), save = TRUE, 
+                          file = "CpG_Totals.pdf", width = 11, height = 4.25, verbose = TRUE){
         if(verbose){
-                message("[plotCpGfilterTotals] Plotting CpG filter totals")
+                message("[plotCpGtotals] Plotting CpG totals")
         }
-        CpGfilterTotals$perSample <- CpGfilterTotals$perSample * 100
-        gg <- ggplot(data = CpGfilterTotals)
+        CpGtotals$perSample <- CpGtotals$perSample * 100
+        gg <- ggplot(data = CpGtotals)
         gg <- gg +
                 geom_line(aes(x = perSample, y = nCpGs_M, group = cov, color = cov)) +
-                geom_text(data = subset(CpGfilterTotals, perSample == min(perSample)), 
+                geom_text(data = subset(CpGtotals, perSample == min(perSample)), 
                           aes(x = perSample, y = nCpGs_M, group = cov, color = cov, label = cov), 
                           size = 4.5, check_overlap = TRUE, nudge_x = -0.5, hjust = 1) +
                 xlab("Samples (%) Cutoff") +
@@ -86,7 +86,7 @@ plotCpGfilterTotals <- function(CpGfilterTotals, nBreaks = 4, legend.position = 
                       panel.grid = element_blank(), plot.margin = unit(c(1,7,0.7,0.7), "lines"))
         if(save){
                 if(verbose){
-                        message("[plotCpGfilterTotals] Saving plot as ", file)
+                        message("[plotCpGtotals] Saving plot as ", file)
                 }
                 ggsave(filename = file, plot = gg, dpi = 600, width = width, height = height, units = "in")
         }
@@ -217,45 +217,45 @@ plotSDstats <- function(regions, bins = 100, nBreaks = 4, legend.position = c(1.
         return(gg)
 }
 
-.regionFilterTotals <- function(regions, covMin, methSD){
+.regionTotals <- function(regions, covMin, methSD){
         regions <- regions[regions$covMin >= covMin & regions$methSD >= methSD,]
         totals <- c("covMin" = covMin, "methSD" = methSD, "totalRegions_K" = nrow(regions)/10^3, 
                     "totalWidth_Mb" = sum(regions$width)/10^6, "totalN_M" = sum(regions$n)/10^6)
         return(totals)
 }
 
-getRegionFilterTotals <- function(regions, covMin = seq(0,20,2), methSD = seq(0,0.1,0.01),
-                                  save = TRUE, file = "Region_Filter_Totals.txt", verbose = TRUE){
+getRegionTotals <- function(regions, covMin = seq(0,20,2), methSD = seq(0,0.1,0.01),
+                            save = TRUE, file = "Region_Totals.txt", verbose = TRUE){
         if(verbose){
-                message("[getRegionFilterTotals] Calculating region totals at specified covMin and methSD cutoffs")
+                message("[getRegionTotals] Calculating region totals at specified covMin and methSD cutoffs")
         }
         covMin <- rep(covMin, each = length(methSD))
         methSD <- rep(methSD, times = length(covMin))
-        regionFilterTotals <- mapply(FUN = .regionFilterTotals, covMin = covMin, methSD = methSD, 
-                                     MoreArgs = list(regions = regions)) %>% t() %>% as.data.frame()
+        regionTotals <- mapply(FUN = .regionTotals, covMin = covMin, methSD = methSD, 
+                               MoreArgs = list(regions = regions)) %>% t() %>% as.data.frame()
         if(save){
                 if(verbose){
-                        message("[getRegionFilterTotals] Saving file as ", file)
-                        write.table(regionFilterTotals, file = file, sep = "\t", row.names = FALSE)
+                        message("[getRegionTotals] Saving file as ", file)
+                        write.table(regionTotals, file = file, sep = "\t", row.names = FALSE)
                 }
         }
-        return(regionFilterTotals)
+        return(regionTotals)
 }
 
-plotRegionFilterTotals <- function(regionFilterTotals, nBreaks = 4, legend.position = c(1.08,0.897), save = TRUE, 
-                                   file = "Region_Filter_Totals.pdf", width = 11, height = 11, verbose = TRUE){
+plotRegionTotals <- function(regionTotals, nBreaks = 4, legend.position = c(1.08,0.897), save = TRUE, 
+                             file = "Region_Totals.pdf", width = 11, height = 11, verbose = TRUE){
         if(verbose){
-                message("[plotRegionFilterTotals] Plotting region filter totals")
+                message("[plotRegionTotals] Plotting region totals")
         }
-        regionFilterTotals <- reshape2::melt(regionFilterTotals, id.vars = c("covMin", "methSD"))
-        regionFilterTotals$variable <- as.character(regionFilterTotals$variable) %>% 
+        regionTotals <- reshape2::melt(regionTotals, id.vars = c("covMin", "methSD"))
+        regionTotals$variable <- as.character(regionTotals$variable) %>% 
                 str_replace_all(pattern = c("totalRegions_K" = "Total Regions (Thousands)", "totalWidth_Mb" = "Total Width (Mb)", 
                                             "totalN_M" = "Total CpGs (Millions)")) %>%
                 factor(levels = c("Total Regions (Thousands)", "Total Width (Mb)", "Total CpGs (Millions)"))
-        gg <- ggplot(data = regionFilterTotals)
+        gg <- ggplot(data = regionTotals)
         gg <- gg +
                 geom_line(aes(x = methSD, y = value, group = covMin, color = covMin)) +
-                geom_text(data = subset(regionFilterTotals, methSD == min(methSD)), 
+                geom_text(data = subset(regionTotals, methSD == min(methSD)), 
                           aes(x = methSD, y = value, group = covMin, color = covMin, label = covMin), 
                           size = 4.5, check_overlap = TRUE, nudge_x = -0.001, hjust = 1) +
                 facet_wrap(vars(variable), nrow = 3, ncol = 1, scales = "free_y", strip.position = "left") +
@@ -275,7 +275,7 @@ plotRegionFilterTotals <- function(regionFilterTotals, nBreaks = 4, legend.posit
                       strip.switch.pad.wrap = unit(0, "lines"), strip.text = element_text(size = 18))
         if(save){
                 if(verbose){
-                        message("[plotRegionFilterTotals] Saving plot as ", file)
+                        message("[plotRegionTotals] Saving plot as ", file)
                 }
                 ggsave(filename = file, plot = gg, dpi = 600, width = width, height = height, units = "in")
         }
@@ -511,8 +511,8 @@ colData <- read.xlsx("sample_info.xlsx", rowNames = TRUE) %>% subset(Sex == "M")
 bs <- getCpGs(colData)
 
 # Examine CpG Totals at Different Cutoffs ####
-CpGfilterTotals <- getCpGfilterTotals(bs)
-plotCpGfilterTotals(CpGfilterTotals)
+CpGtotals <- getCpGtotals(bs)
+plotCpGtotals(CpGtotals)
 
 # Filter BSobject ####
 bs <- filterCpGs(bs, cov = 2, perSample = 0.75)
@@ -523,8 +523,8 @@ plotRegionStats(regions, file = "Unfiltered_Region_Plots.pdf")
 plotSDstats(regions, file = "Unfiltered_SD_Plots.pdf")
 
 # Examine Region Totals at Different Cutoffs ####
-regionFilterTotals <- getRegionFilterTotals(regions)
-plotRegionFilterTotals(regionFilterTotals)
+regionTotals <- getRegionTotals(regions)
+plotRegionTotals(regionTotals)
 
 # Call Regions with Filtering ####
 regions <- getRegions(bs, covMin = 10, methSD = 0.05)
