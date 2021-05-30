80212179573f6120c66c7302ed6c53619a3ddf35
Charles Mordaunt <cemordaunt@ucdavis.edu>
2021-05-30 17:03:03 -0400

    Add package documentation

---
+++ R/comethyl.R
@@ -0,0 +1,20 @@
+#' comethyl: An R package for weighted region comethylation network analysis
+#'
+#' Comethyl builds upon the \code{WGCNA} package to identify and interpret modules
+#' of comethylated regions from whole-genome bisulfite sequencing data. Regions
+#' are defined from clusters of CpG sites or from genomic annotations, and then
+#' percent methylation values are used to identify comethylation modules.
+#' Interesting modules are identified and explored by comparing with sample traits
+#' and examining functional enrichments. Results are then visualized with
+#' high-quality, editable plots from \code{ggplot2}.
+#'
+#' @author
+#' Charles Mordaunt
+#'
+#' @seealso
+#' GitHub: \url{https://github.com/cemordaunt/comethyl}
+#'
+#' @docType package
+#' @name comethyl
+NULL
+
